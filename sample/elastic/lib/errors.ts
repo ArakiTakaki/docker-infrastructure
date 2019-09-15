662df204bb0c30ce60617ced328658a8b28decec
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-09-15 13:59:42 +0900

    elasticsearchのサンプル

---
+++ sample/elastic/lib/errors.ts
@@ -0,0 +1,12 @@
+export const ErrorConsole = (err: any) => {
+  console.log('================ ERROR ================')
+  console.log('================ ERROR ================')
+  console.log('================ ERROR ================')
+  console.log('================ ERROR ================')
+  console.error(err);
+  console.log('================ ERROR ================')
+  console.log('================ ERROR ================')
+  console.log('================ ERROR ================')
+  console.log('================ ERROR ================')
+  console.log('================ ERROR ================')
+}
---
+++ sample/elastic/main.ts
@@ -0,0 +1,57 @@
+import { Client } from '@elastic/elasticsearch';
+import { ErrorConsole } from './lib/errors';
+const client = new Client({ node: 'http://localhost:9200' })
+
+// インデックスの作成サンプル
+const main = async () => {
+  try {
+    // Let's start by indexing some data
+    await client.index({
+      index: 'game-of-thrones',
+      // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+      body: {
+        character: 'Ned Stark',
+        quote: 'Winter is coming.'
+      }
+    })
+
+    await client.index({
+      index: 'game-of-thrones',
+      // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+      body: {
+        character: 'Daenerys Targaryen',
+        quote: 'I am the blood of the dragon.'
+      }
+    })
+
+    await client.index({
+      index: 'game-of-thrones',
+      // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+      body: {
+        character: 'Tyrion Lannister',
+        quote: 'A mind needs books like a sword needs a whetstone.'
+      }
+    })
+
+    // here we are forcing an index refresh, otherwise we will not
+    // get any result in the consequent search
+    await client.indices.refresh({ index: 'game-of-thrones' })
+
+    // Let's search!
+    const { body } = await client.search({
+      index: '*',
+      // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+      body: {
+        // query: {
+        //   // match: { quote: '' }
+        // }
+      }
+    })
+
+    console.log(body.hits.hits)
+  } catch (err) {
+    ErrorConsole(err);
+  }
+}
+
+main();
